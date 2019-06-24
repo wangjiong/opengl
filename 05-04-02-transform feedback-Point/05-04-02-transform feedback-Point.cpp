18b7142fc25f13dd3ce8c3ffa86efb14242cd907
johnjwang <johnjwang@tencent.com>
2019-06-24 20:05:08 +0800

    完善transform feedback

--- 05-04-02-transform feedback-Point/05-04-02-transform feedback-Point.cpp
+++ 05-04-02-transform feedback-Point/05-04-02-transform feedback-Point.cpp
@@ -9,6 +9,8 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
 
 int main() {
+	
+
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -27,6 +29,10 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	// 点的大小
+	glPointSize(10);
+
 	// 顶点
 	float vertices[] = {
 		0.0f, 0.0f,
@@ -35,61 +41,46 @@ int main() {
 		0.1f, 0.1f,
 	};
 
-	const int col = 2;
-	const int row = 2;
-	//int index = 0;
-	//float* pVertices = new float[col * row * 2];
-	//for( int i = 0; i < col; i++ )
-	//{
-	//	for( int j = 0; j < row; j++ )
-	//	{
-	//		pVertices[index++] = i * 0.1f;
-	//		pVertices[index++] = j * 0.1f;
+	const int col = 5;
+	const int row = 5;
+	int index = 0;
+	float* pVertices = new float[col * row * 2];
+	for( int i = 0; i < col; i++ )
+	{
+		for( int j = 0; j < row; j++ )
+		{
+			pVertices[index++] = i * 0.1f;
+			pVertices[index++] = j * 0.1f;
 
-	//		//printf("%f %f\n" , i * 0.1f , j * 0.1f);
-	//	}
-	//}
+			//printf("%f %f\n" , i * 0.1f , j * 0.1f);
+		}
+	}
 
 	// VAO
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	// VBO
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	//glBufferData(GL_ARRAY_BUFFER, col * row * 2 * sizeof(float), pVertices, GL_STATIC_DRAW);
-	// 顶点规范
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+	unsigned int vaos[2];
+	glGenVertexArrays( 2, vaos );
+	unsigned int vbos[2];
+	glGenBuffers( 2, vbos );
+	for( int i = 0; i < 2; i++ )
+	{
+		glBindVertexArray( vaos[i] );
+		glBindBuffer( GL_ARRAY_BUFFER, vaos[i] );
+		//glBufferData( GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW );
+		glBufferData(GL_ARRAY_BUFFER, col * row * 2 * sizeof(float), pVertices, GL_STATIC_DRAW);
+
+		// 顶点规范
+		glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof( float ), (void*)0 );
+		glEnableVertexAttribArray( 0 );
+	}
 	// Shader
 	Shader shader("shader.vs", "shader.fs");
-
 	shader.use();
+
 	const GLchar* feedbackVaryings[] = { "outValue" };
 	glTransformFeedbackVaryings( shader.ID, 1, feedbackVaryings, GL_INTERLEAVED_ATTRIBS );
 	glLinkProgram( shader.ID );
 
-	// 点的大小
-	glPointSize(10);
-
-	///////////////////////
-	unsigned int VAO1;
-	glGenVertexArrays(1, &VAO1);
-	glBindVertexArray(VAO1);
-
-	GLuint tbo;
-	glGenBuffers(1, &tbo);
-	glBindBuffer(GL_ARRAY_BUFFER, tbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_READ);
-
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	///////////////////////
-
-	int i = 0;
+	index = 0;
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -97,16 +88,9 @@ int main() {
 
 		shader.use();
 
-		if( i++ % 2 == 0 )
-		{
-			glBindVertexArray( VAO );
-			glBindBufferBase( GL_TRANSFORM_FEEDBACK_BUFFER, 0, tbo );
-		}
-		else
-		{
-			glBindVertexArray( VAO1 );
-			glBindBufferBase( GL_TRANSFORM_FEEDBACK_BUFFER, 0, VBO );
-		}
+		int i = index++ % 2;
+		glBindVertexArray( vaos[i] );
+		glBindBufferBase( GL_TRANSFORM_FEEDBACK_BUFFER, 0, vbos[1 - i] );
 
 		
 		glBeginTransformFeedback(GL_POINTS);
--- 05-04-02-transform feedback-Point/shader.vs
+++ 05-04-02-transform feedback-Point/shader.vs
@@ -6,7 +6,7 @@ out vec2 outValue;
 void main()
 {
 	outValue.x = aPos.x + 0.001;
-	outValue.y = aPos.y;
+	outValue.y = aPos.y + 0.001;
 
     gl_Position = vec4(aPos, 1.0, 1.0);
 }
